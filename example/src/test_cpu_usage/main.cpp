31b19feb8a2346983df1ab0babc836732fe085f7
ubuntu <>
2025-06-22 05:40:21 +0000

    cleanup

--- example/src/test_cpu_usage/main.cpp
+++ example/src/test_cpu_usage/main.cpp
@@ -2,20 +2,7 @@
 
 namespace ccapi {
 
-class MyLogger final : public Logger {
- public:
-  void logMessage(const std::string& severity, const std::string& threadId, const std::string& timeISO, const std::string& fileName,
-                  const std::string& lineNumber, const std::string& message) override {
-    std::lock_guard<std::mutex> lock(m);
-    std::cout << threadId << ": [" << timeISO << "] {" << fileName << ":" << lineNumber << "} " << severity << std::string(8, ' ') << message << std::endl;
-  }
-
- private:
-  std::mutex m;
-};
-
-MyLogger myLogger;
-Logger* Logger::logger = &myLogger;
+Logger* Logger::logger = nullptr;  // This line is needed.
 
 class MyEventHandler : public EventHandler {
  public:
